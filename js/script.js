9d3fef8b92584d68577d2b919d87f02b4a643821
syfjasm27 <syfjasm27@gmail.com>
2020-06-19 02:40:58 +0300

    create project

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+
+
+// function changecolor(){
+
+// var x=document.getElementById("first-site");
+// if(x.style.color =="white")
+// 	x.style.color="deepskyblue";
+
+// else if(x.style.color =="deepskyblue")
+// 	x.style.color="white";
+
+
+
+// else
+// 	x.style.color="white";	
+
+
+// var y=document.getElementById("last-site");
+// if(y.style.color =="white")
+// 	y.style.color="deepskyblue";
+
+// else if(y.style.color =="deepskyblue")
+// 	y.style.color="white";
+
+
+
+
+// else
+// 	y.style.color="white";
+
+// }
